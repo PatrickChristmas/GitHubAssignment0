78b87cbb82248fa86124e0f1d31e0a49a9d9f1e5
pac62 <pac62@192.168.1.222>
2024-11-05 15:25:25 -0500

    Commit 2

--- GithubTestProject/src/HelloWorld.java
+++ GithubTestProject/src/HelloWorld.java
@@ -19,7 +19,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Change Number 1");
 
 	}
 
